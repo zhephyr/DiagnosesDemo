c5c2e26e06dccc7a9ddfb3c0cdeb4fc324ed8bb8
zhephyr <wang.zhexi@gmail.com>
2017-08-11 18:04:44 -0500

    Picker Layout set

--- DiagnosesDemo/DiagnosesDemo/wwwroot/js/pickerApp.js
+++ DiagnosesDemo/DiagnosesDemo/wwwroot/js/pickerApp.js
@@ -1,6 +1,48 @@
 ﻿'use strict';
 
-var app = angular.module('pickerApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
-app.controller('pickerCtrl', function ($scope, $http, bsLoadingOverlayService) {
+var app = angular.module('pickerApp', ['ngAnimate', 'ui.bootstrap'])
+app.controller('pickerCtrl', function ($scope, $http) {
+	$scope.product = {
+		main_cat: "",
+		prod_type: "",
+		diagnosis: "",
+		hcpcs: ""
+	}
 
-}
+	$scope.placehold = {
+		main_cat: "Patient Type?",
+		prod_type: "Product Type?",
+		diagnosis: "Current Diagnosis?"
+	}
+
+	$scope.head_status = {
+		first: true,
+		second: false,
+		third: false
+	}
+
+	$scope.$watch('product', function (newVal, oldVal) {
+		for (var prop in $scope.product) {
+			if (newVal[prop] !== oldVal[prop]) {
+				switch (prop) {
+					case 'main_cat':
+						$scope.head_status = {
+							first: false,
+							second: true,
+							third: false
+						}
+						break;
+					case 'prod_type':
+						$scope.head_status = {
+							first: false,
+							second: false,
+							third: true
+						}
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}, true);
+});
