2624130db20764b75dcbfd01c76de5db57096592
zhephyr <wang.zhexi@gmail.com>
2017-08-10 19:06:46 -0500

    More Frameworks added.

---
+++ DiagnosesDemo/DiagnosesDemo/wwwroot/js/pickerApp.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+var app = angular.module('pickerApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
+app.controller('pickerCtrl', function ($scope, $http, bsLoadingOverlayService) {
+
+}
