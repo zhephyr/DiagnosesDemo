8b858b70388c9be131c5d288ce2f4528c75e694e
Michael Wang <wang.michael619@gmail.com>
2019-04-16 18:09:35 -0500

    Added image files for budles. Added question slides.

--- DiagnosesDemo/DiagnosesDemo/wwwroot/js/sliderApp.js
+++ DiagnosesDemo/DiagnosesDemo/wwwroot/js/sliderApp.js
@@ -12,7 +12,7 @@ app.controller('sliderApp', function ($scope, $location, $anchorScroll, $http) {
     $scope.currTempIdx = 0;
 
     $scope.bundle_answers = { weight_shift: 'init', outdoors: 'init' };
-    
+
     $(function () {
         for (var i = 1; i < $scope.questionUrls.length; i++) {
             var width = $('[id="slide' + i + '"]').width();
@@ -80,43 +80,22 @@ app.controller('sliderApp', function ($scope, $location, $anchorScroll, $http) {
         }
     };
 
-    $scope.popIn = function ($element) {
-        $element.css({ transform: "scale(1,1)" });
-    };
+    $scope.selectBubble = function (idx, pop) {
+        if (idx !== 0) {
+            var $element = $($('.img-bubble')[pop]);
+            var $main = $($('.img-bubble')[2]);
 
-    $scope.popOut = function ($element) {
-        $element.css({ transform: "scale(0,0)" });
-    };
-
-    $scope.selectBubble = function (idx) {
-        var $element = $($('.img-bubble')[idx]);
-        var $main = $($('.img-bubble')[2]);
-
-        $element.css({ transform: "scale(0,0)" }).promise().done(function () {
-            $main.css({ transform: "scale(0,0)" }).promise().done(function () {
-                [colors[idx], colors[2]] = [colors[2], colors[idx]];
+            $element.css({ transform: "scale(0,0)" }).promise().done(function () {
+                $main.css({ transform: "scale(0,0)" }).promise().done(function () {
+                    [$scope.show_bundles[idx], $scope.show_bundles[0]] = [$scope.show_bundles[0], $scope.show_bundles[idx]];
+                });
             });
-        });
 
-        setTimeout(function () {
-            $scope.colorBubbles();
-            $element.css({ transform: "scale(1,1)" }).promise().done(function () {
-                $main.css({ transform: "scale(1,1)" });
-            });
-        }, 1000);
-    };
-
-    var colors = ["red", "yellow", "green", "blue", "violet"];
-
-    angular.element(document).ready(function () {
-        $scope.colorBubbles();
-    });
-
-    $scope.colorBubbles = function () {
-        var bubbles = $('.img-bubble');
-        for (var i = 0; i < bubbles.length; i++) {
-            var $bubble = $(bubbles[i]);
-            $bubble.css({ 'background-color': colors[i] });
+            setTimeout(function () {
+                $element.css({ transform: "scale(1,1)" }).promise().done(function () {
+                    $main.css({ transform: "scale(1,1)" });
+                });
+            }, 1000);
         }
     };
 
@@ -160,6 +139,76 @@ app.controller('sliderApp', function ($scope, $location, $anchorScroll, $http) {
                     }
                 }
             }
+
+            $scope.$digest();
         }
     }, true);
+
+    $scope.$watch('bundle_answers.outdoors', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.bundles = full_bundle_list.slice();
+
+            if (newVal) {
+                for (var i = $scope.bundles.length - 1; i >= 0; i--) {
+                    if ($scope.bundles[i].indoor_outdoor === "indoor") {
+                        $scope.bundles.splice(i, 1);
+                    }
+                }
+            } else {
+                for (var i = $scope.bundles.length - 1; i >= 0; i--) {
+                    if ($scope.bundles[i].indoor_outdoor === "outdoor") {
+                        $scope.bundles.splice(i, 1);
+                    }
+                }
+            }
+
+            if ($scope.bundle_answers.weight_shift === 'yes') {
+                for (var i = $scope.bundles.length - 1; i >= 0; i--) {
+                    if ($scope.bundles[i].chair_func.includes("power")) {
+                        $scope.bundles.splice(i, 1);
+                    }
+                }
+            } else if (!$scope.bundle_answers.weight_shift === 'no') {
+                for (var i = $scope.bundles.length - 1; i >= 0; i--) {
+                    if (!$scope.bundles[i].chair_func.includes("power")) {
+                        $scope.bundles.splice(i, 1);
+                    }
+                }
+            }
+
+            $scope.$digest();
+        }
+    }, true);
+
+    var product_images_list = [
+        { name: "Corpus Ergo Backrest", img_src: "Corpus Back & Seat Cushion.jpg" },
+        { name: "Permobil F3 Power Wheelchair", img_src: "F3.png" },
+        { name: "ROHO Quadtro Select High Profile Seat Cushion", img_src: "ROHO Quadtro Select_HP.jpg" },
+        { name: "ROHO Agility Max Contour Backrest", img_src: "AG6 Back Angled QR LG.jpg" },
+        { name: "Comfort M2 Seat Cushion", img_src: "M2 Zero 1115.jpg" },
+        { name: "ROHO Hybrid Elite Seat Cushion", img_src: "Hybrid Elite Dual Valve_No Bkgrd_No Cov.jpg" },
+        { name: "Corpus Ergo Seat cushion", img_src: "Corpus Back & Seat Cushion.jpg" },
+        { name: "Permobil M3 Power Wheelchair", img_src: "M3.png" },
+        { name: "Permobil F5 VS Power Wheelchair", img_src: "F5VS.png" },
+        { name: "Permobil M1 Power Wheelchair", img_src: "M1.png" },
+        { name: "ROHO Agility Min Countour Backrest W/ Air and Lumbar", img_src: "AG2 Back Angled LG.jpg" }
+    ];
+
+    $scope.$watch('bundles', function (newVal, oldVal) {
+        if (newVal) {
+            var show_bundles = newVal.slice(0, 5);
+            show_bundles.forEach(function (bundle) {
+                bundle.products.forEach(function (product) {
+                    product.img_src = product_images_list.find(function (element) {
+                        if (element.name == product.name) {
+                            return element;
+                        }
+                    }).img_src;
+                });
+            });
+
+            $scope.show_bundles = show_bundles;
+            $scope.$apply();
+        }
+    });
 });
